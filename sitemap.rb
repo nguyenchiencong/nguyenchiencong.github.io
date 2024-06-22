a5f9c0a735902d3016c5255664a65adced9ff785
Canopus <nguyen.chiencong@gmail.com>
2024-06-22 20:52:29 +0700

    update

--- sitemap.rb
+++ sitemap.rb
@@ -6,6 +6,7 @@ SitemapGenerator::Sitemap.create do
   add '/', :priority => 1
   add '/vi', :priority => 1
   add '/fr', :priority => 1
+  add '/dao', :priority => 1
   add '/now', :priority => 0.8
   add '/books', :priority => 0.8
   add '/game', :priority => 0.8
